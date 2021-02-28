16719d151e8cf9b56a851d2164e27f5fa035485c
German Ubushaev <repmah@gmail.com>
2021-02-28 10:28:18 +0300

    working prototype

---
+++ HashCheck/HashValidator.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace HashCheck
+{
+    public static class HashValidator
+    {
+        public static HashAlgorithm ParseHashAlgorithm(string str)
+        {
+            return str switch
+            {
+                "sha1" => HashAlgorithm.Sha1,
+                "sha256" => HashAlgorithm.Sha256,
+                "md5" => HashAlgorithm.Md5,
+                _ => throw new ArgumentOutOfRangeException(nameof(str), str, null)
+            };
+        }
+
+        public static HashValidationResult Validate(FileHashInfo info, DirectoryInfo filesToValidateDirectory)
+        {
+            var name = info.FileName;
+
+            var file = new FileInfo(Path.Combine(filesToValidateDirectory.FullName, name));
+
+            if (!file.Exists)
+            {
+                return HashValidationResult.FileNotFound;
+            }
+
+            var bytes = File.ReadAllBytes(file.FullName);
+            var hash = GetHash(bytes, info.HashAlgorithm);
+
+            return info.ExpectedHashValue == hash
+                ? HashValidationResult.HashCorrect
+                : HashValidationResult.HashMismatch;
+        }
+
+        private static string GetHash(byte[] inputBytes, HashAlgorithm hashAlgorithm)
+        {
+            var hashValue = hashAlgorithm switch
+            {
+                HashAlgorithm.Sha1 => SHA1.Create().ComputeHash(inputBytes),
+                HashAlgorithm.Sha256 => SHA256.Create().ComputeHash(inputBytes),
+                HashAlgorithm.Md5 => MD5.Create().ComputeHash(inputBytes),
+                _ => throw new ArgumentOutOfRangeException(nameof(hashAlgorithm), hashAlgorithm, null)
+            };
+
+            var hashString = BitConverter.ToString(hashValue).Replace("-", "").ToLower();
+            return hashString;
+        }
+    }
+
+    public enum HashAlgorithm
+    {
+        Sha1,
+        Sha256,
+        Md5
+    }
+
+    public enum HashValidationResult
+    {
+        HashCorrect,
+        HashMismatch,
+        FileNotFound
+    }
+
+    public struct FileHashInfo
+    {
+        public FileHashInfo(string fileName, HashAlgorithm hashAlgorithm, string expectedHashValue)
+        {
+            FileName = fileName;
+            HashAlgorithm = hashAlgorithm;
+            ExpectedHashValue = expectedHashValue;
+        }
+
+        public string FileName { get; }
+        public HashAlgorithm HashAlgorithm { get; }
+        public string ExpectedHashValue { get; }
+    }
+}
---
+++ HashCheck/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace HashCheck
+{
+    internal static class Program
+    {
+        private static int Main(string[] args)
+        {
+            try
+            {
+                var textFilePath = args[0];
+                var directoryPath = args[1];
+
+                var filesToValidateDirectory = new DirectoryInfo(directoryPath);
+
+                var fileSpecs = File.ReadAllLines(textFilePath)
+                    .Select(
+                        file =>
+                        {
+                            var split = file.Split(' ');
+                            return new FileHashInfo(split[0], HashValidator.ParseHashAlgorithm(split[1]), split[2]);
+                        }
+                    );
+
+                foreach (var spec in fileSpecs)
+                {
+                    var result = HashValidator.Validate(spec, filesToValidateDirectory);
+                    var resultString = result switch
+                    {
+                        HashValidationResult.HashCorrect => "OK",
+                        HashValidationResult.HashMismatch => "FAIL",
+                        HashValidationResult.FileNotFound => "NOT FOUND",
+                        _ => throw new ArgumentOutOfRangeException(nameof(result))
+                    };
+
+                    Console.WriteLine($"{spec.FileName}: {resultString}");
+                }
+
+                return 0;
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine(
+                    "Wrong input. You should provide a path to the file with the hash data and a path to the directory. Please, try again.");
+
+                return 1;
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("One of the paths you provided were incorrect. Please, try again.");
+
+                return 1;
+            }
+            // catch (InvalidDataException)
+            // {
+            //     Console.WriteLine("Your hash data file is incorrect/badly formatted. Please, try again.");
+            // }
+            catch (Exception)
+            {
+                Console.WriteLine("Something went horribly wrong.");
+
+                return 1;
+            }
+        }
+    }
+}
