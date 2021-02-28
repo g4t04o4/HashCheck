c7705b74bec24ada2bece68ffa0025abef069f2f
g4t04o4 <repmah@gmail.com>
2021-02-28 11:05:08 +0300

    Polished exception handling.

--- HashCheck/Program.cs
+++ HashCheck/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -6,63 +7,137 @@ namespace HashCheck
 {
     internal static class Program
     {
-        private static int Main(string[] args)
+        private class AppOptions
         {
+            public readonly FileInfo SpecsFile;
+            public readonly DirectoryInfo FilesForValidationDirectory;
+
+            public AppOptions(FileInfo specsFile, DirectoryInfo filesForValidationDirectory)
+            {
+                SpecsFile = specsFile;
+                FilesForValidationDirectory = filesForValidationDirectory;
+            }
+        }
+
+        private class CommandLineArgumentsParseResult
+        {
+            public bool IsSuccess { get; }
+            public AppOptions Options { get; }
+            public string FailReason { get; }
+
+            public CommandLineArgumentsParseResult(AppOptions options)
+            {
+                IsSuccess = true;
+                Options = options;
+            }
+
+            public CommandLineArgumentsParseResult(string failReason)
+            {
+                IsSuccess = false;
+                FailReason = failReason;
+            }
+        }
+
+        private static CommandLineArgumentsParseResult TryParseCommandLineArguments(IReadOnlyList<string> args)
+        {
+            if (args.Count != 2)
+            {
+                return new CommandLineArgumentsParseResult($"Two command line arguments expected, got {args.Count}.");
+            }
+
+            if (string.IsNullOrWhiteSpace(args[0]))
+            {
+                return new CommandLineArgumentsParseResult("First argument was empty.");
+            }
+
+            if (string.IsNullOrWhiteSpace(args[1]))
+            {
+                return new CommandLineArgumentsParseResult("Second argument was empty.");
+            }
+
+            FileInfo specsFile;
             try
             {
-                var textFilePath = args[0];
-                var directoryPath = args[1];
-
-                var filesToValidateDirectory = new DirectoryInfo(directoryPath);
-
-                var fileSpecs = File.ReadAllLines(textFilePath)
-                    .Select(
-                        file =>
-                        {
-                            var split = file.Split(' ');
-                            return new FileHashInfo(split[0], HashValidator.ParseHashAlgorithm(split[1]), split[2]);
-                        }
-                    );
-
-                foreach (var spec in fileSpecs)
-                {
-                    var result = HashValidator.Validate(spec, filesToValidateDirectory);
-                    var resultString = result switch
-                    {
-                        HashValidationResult.HashCorrect => "OK",
-                        HashValidationResult.HashMismatch => "FAIL",
-                        HashValidationResult.FileNotFound => "NOT FOUND",
-                        _ => throw new ArgumentOutOfRangeException(nameof(result))
-                    };
-
-                    Console.WriteLine($"{spec.FileName}: {resultString}");
-                }
-
-                return 0;
+                specsFile = new FileInfo(args[0]);
             }
-            catch (IndexOutOfRangeException)
-            {
-                Console.WriteLine(
-                    "Wrong input. You should provide a path to the file with the hash data and a path to the directory. Please, try again.");
-
-                return 1;
-            }
-            catch (FileNotFoundException)
-            {
-                Console.WriteLine("One of the paths you provided were incorrect. Please, try again.");
-
-                return 1;
-            }
-            // catch (InvalidDataException)
-            // {
-            //     Console.WriteLine("Your hash data file is incorrect/badly formatted. Please, try again.");
-            // }
             catch (Exception)
             {
-                Console.WriteLine("Something went horribly wrong.");
+                return new CommandLineArgumentsParseResult("Specs file path was provided in incorrect format.");
+            }
 
+            DirectoryInfo filesToValidateDirectory;
+            try
+            {
+                filesToValidateDirectory = new DirectoryInfo(args[1]);
+            }
+            catch (Exception)
+            {
+                return new CommandLineArgumentsParseResult("Specs file path was provided in incorrect format.");
+            }
+
+            if (!specsFile.Exists)
+            {
+                return new CommandLineArgumentsParseResult("Specs file does not exist.");
+            }
+
+            if (!filesToValidateDirectory.Exists)
+            {
+                return new CommandLineArgumentsParseResult(
+                    "Provided directory with files for validation does not exist.");
+            }
+
+            return new CommandLineArgumentsParseResult(new AppOptions(specsFile, filesToValidateDirectory));
+        }
+
+        private static void Run(AppOptions options)
+        {
+            var fileSpecs = File.ReadAllLines(options.SpecsFile.FullName)
+                .Select(
+                    file =>
+                    {
+                        var split = file.Split(' ');
+                        return new FileHashInfo(split[0], HashValidator.ParseHashAlgorithm(split[1]), split[2]);
+                    }
+                );
+
+            foreach (var spec in fileSpecs)
+            {
+                var result = HashValidator.Validate(spec, options.FilesForValidationDirectory);
+                var resultString = result switch
+                {
+                    HashValidationResult.HashCorrect => "OK",
+                    HashValidationResult.HashMismatch => "FAIL",
+                    HashValidationResult.FileNotFound => "NOT FOUND",
+                    _ => throw new ArgumentOutOfRangeException(nameof(result))
+                };
+
+                Console.WriteLine($"{spec.FileName}: {resultString}");
+            }
+        }
+
+        private static int Main(string[] args)
+        {
+            var parsingResult = TryParseCommandLineArguments(args);
+            if (!parsingResult.IsSuccess)
+            {
+                Console.Error.WriteLine("Error occured while parsing command line arguments: ");
+                Console.Error.WriteLine(parsingResult.FailReason);
                 return 1;
             }
+
+            var options = parsingResult.Options;
+            try
+            {
+                Run(options);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine("Error occured while running application:");
+                Console.Error.WriteLine(ex.Message);
+                return 1;
+            }
+
+            return 0;
         }
     }
 }
